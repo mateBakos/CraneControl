3f85e62b21ed5139d53af26dc967ad3d5ec89b87
Thomas Booij <thomasbooij95@gmail.com>
2018-05-23 13:24:22 +0200

    import data to mfile

--- HelpFromWebsite/m-files/State_space_crane.m
+++ HelpFromWebsite/m-files/State_space_crane.m
@@ -1,10 +1,24 @@
+%% Import Simulation data;
 close all
+simData = loadfiles('initial_angle_14.mat');
+%plot_timeseries(simData)
+beginSample  = 162;
+timevector   =  simData.Time(beginSample:end);
+positiondata = -simData.Data(beginSample:end,1);
+angledata    = -simData.Data(beginSample:end,5);
+figure(1)
+subplot(2,1,1)
+plot(timevector,positiondata)
+subplot(2,1,2)
+plot(timevector,angledata)
+
+%% Building Model
 M   = 0.95; % Trolley mass [kg]
 m   = 1.30; % Container mass [kg]
 L   = 1;   % Cable length [m]
 k_m = -10;  % Nm
 b_x = 10;  % kgs-1
-b_phi = 2; %pdf value: 0.75;% kgs-1
+b_phi = 0.9; %pdf value: 0.75;% kgs-1
 g = 9.81;
 
 A = [0,0,1,0;
@@ -15,10 +29,10 @@ B = [0;0;1/M;1/(M*L)];
 C = [1,0,0,0;
     0,1,0,0];
 D = [0;0];
+% figure
+ Crane = ss(A,B,C,D);
+% step(Crane);
+% figure
+% impulse(Crane);
 figure
-Crane = ss(A,B,C,D);
-step(Crane);
-figure
-impulse(Crane);
-figure
-initial(Crane,[0,pi/4,0,0])
+initial(Crane,[0,deg2rad(10),0,0])
