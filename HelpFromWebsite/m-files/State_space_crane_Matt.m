db1d2eb89dcd3925ace87f205c4e7e93628ce3dd
NemeZisHun <bakos.mate94@gmail.com>
2018-05-27 14:20:44 +0200

    Changed segway simulink model, can't run on my version

--- HelpFromWebsite/m-files/State_space_crane_Matt.m
+++ HelpFromWebsite/m-files/State_space_crane_Matt.m
@@ -3,7 +3,8 @@ close all
 clear all
 clc
 
-simData = loadfiles('Signal_xSin_yCos_4.mat');
+simData = loadfiles('Bernoulli_1m_10.mat');
+%'Bernoulli_1m_9.mat'
 %'Signal_xSin_yCos_4.mat'
 %'initial_angle_14.mat'
 plot_timeseries(simData)
@@ -42,66 +43,52 @@ g = 9.81;
 Ts=0.01;
 
 data = iddata([position,angle,cable],[inputx,inputy],Ts,'Name','Measurement Data',...
-    'OutputName',{'Position','Cable Length','Angle'}, ...
-    'OutputUnit',{'m','m','rad'},...
+    'OutputName',{'Position','Angle','Cable Length'}, ...
+    'OutputUnit',{'m','rad','m'},...
     'InputName',{'x-velocity','y-velocity'}, ...
     'InputUnit',{'m/s','m/s'});
 
-%% Greybox setup
-FileName      = 'GreyBoxDiffModel';        % File describing the model structure.
-Order         = [3 2 4];             % Model orders [ny nu nx].
-Parameters    = [M,m,L,k_m,b_x,b_phi];   % Initial parameters.
-InitialStates = [0,deg2rad(10),0,0]';              % Initial initial states.
-nlgr = idnlgrey(FileName, Order, Parameters, InitialStates, Ts);
-
-nlgr.OutputName = {'Position','Cable Length','Angle'};
-nlgr.OutputUnit = {'m','m','rad'};
-nlgr.TimeUnit = 's';
-nlgr = setinit(nlgr, 'Name', {'Trolley position' 'Container Angle'...
-                    'Trolley velocity' 'Container Angular velocity'});
-nlgr = setinit(nlgr, 'Unit', {'m' 'rad' 'm/s' 'rad/s'});
-nlgr = setpar(nlgr, 'Name', {'Trolley mass' 'Container Mass' ...
-                      'Initial Cable Length' 'Motor Gain' ...
-                      'Trolley damping' 'Cable Damping'});
-nlgr = setpar(nlgr, 'Unit', {'kg' 'kg' 'm' '?' 'kgs-1' 'kgs-1*m'});
-nlgr = setpar(nlgr, 'Minimum', {eps(0) eps(0) eps(0) eps(0) eps(0) eps(0)});   % All parameters > 0.
-
-% A. Model computed with first-order Euler forward ODE solver.
-nlgref = nlgr;
-nlgref.SimulationOptions.Solver = 'ode1';        % Euler forward.
-nlgref.SimulationOptions.FixedStep = Ts*0.1;   % Step size.
-
-% B. Model computed with adaptive Runge-Kutta 23 ODE solver.
-nlgrrk23 = nlgr;
-nlgrrk23.SimulationOptions.Solver = 'ode23';     % Runge-Kutta 23.
-
-% C. Model computed with adaptive Runge-Kutta 45 ODE solver.
-nlgrrk45 = nlgr;
-nlgrrk45.SimulationOptions.Solver = 'ode45';     % Runge-Kutta 45.
-
-figure(1)
-compare(data, nlgref, nlgrrk23, nlgrrk45, 1, ...
-compareOptions('InitialCondition', 'model'));
-
+% %% Greybox setup
+% FileName      = 'GreyBoxDiffModel';        % File describing the model structure.
+% Order         = [3 2 4];             % Model orders [ny nu nx].
+% Parameters    = [M,m,L,k_m,b_x,b_phi];   % Initial parameters.
+% InitialStates = [0,deg2rad(10),0,0]';              % Initial initial states.
+% nlgr = idnlgrey(FileName, Order, Parameters, InitialStates, Ts);
+% 
+% nlgr.OutputName = {'Position','Cable Length','Angle'};
+% nlgr.OutputUnit = {'m','m','rad'};
+% nlgr.TimeUnit = 's';
+% nlgr = setinit(nlgr, 'Name', {'Trolley position' 'Container Angle'...
+%                     'Trolley velocity' 'Container Angular velocity'});
+% nlgr = setinit(nlgr, 'Unit', {'m' 'rad' 'm/s' 'rad/s'});
+% nlgr = setpar(nlgr, 'Name', {'Trolley mass' 'Container Mass' ...
+%                       'Initial Cable Length' 'Motor Gain' ...
+%                       'Trolley damping' 'Cable Damping'});
+% nlgr = setpar(nlgr, 'Unit', {'kg' 'kg' 'm' '?' 'kgs-1' 'kgs-1*m'});
+% nlgr = setpar(nlgr, 'Minimum', {eps(0) eps(0) eps(0) eps(0) eps(0) eps(0)});   % All parameters > 0.
+% 
+% % A. Model computed with first-order Euler forward ODE solver.
+% nlgref = nlgr;
+% nlgref.SimulationOptions.Solver = 'ode1';        % Euler forward.
+% nlgref.SimulationOptions.FixedStep = Ts*0.1;   % Step size.
+% 
+% % B. Model computed with adaptive Runge-Kutta 23 ODE solver.
+% nlgrrk23 = nlgr;
+% nlgrrk23.SimulationOptions.Solver = 'ode23';     % Runge-Kutta 23.
+% 
+% % C. Model computed with adaptive Runge-Kutta 45 ODE solver.
+% nlgrrk45 = nlgr;
+% nlgrrk45.SimulationOptions.Solver = 'ode45';     % Runge-Kutta 45.
+% 
+% figure(1)
+% compare(data, nlgref, nlgrrk23, nlgrrk45, 1, ...
+% compareOptions('InitialCondition', 'model'));
+% 
 
 %Greybox_model = idgrey('LinCraneSS',{M,m,L,k_m,b_x,b_phi},'d');
 
+opt=ssestOptions('InitialState','estimate','EnforceStability',true,'Display','on');
+Blackbox_model = ssest(data,4,opt);
 
-Blackbox_model = ssest(data,4,'InitialState','estimate','Display','on');
-
-
-
-% opt = greyestOptions('InitialState','estimate','Display','on');
-% opt.EnforceStability = true;
-% Greybox_model = greyest(data,Greybox_model,opt);
 figure
- compare(data,Blackbox_model)
-%  
-%  save('initial_angle_14_Blackbox_model.mat','Blackbox_model')
-% 
-% % step(Crane);
-% % figure
-% % impulse(Crane);
-% 
-%  %figure
-%  %initial(Blackbox_model,[0,deg2rad(10),0,0])
+compare(data,Blackbox_model)
