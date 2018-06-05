a938e5f3f989483a3427a2af496302474e7671e9
Thomas Booij <tcbooij@tudelft.net>
2018-06-05 17:32:07 +0200

    change simulink model to use best ssest data

--- HelpFromWebsite/m-files/SimulinkPIDinit.m
+++ HelpFromWebsite/m-files/SimulinkPIDinit.m
@@ -1,8 +1,8 @@
-load('Blackbox4thorder')
+load('BlackBoxID_meas9_order4')
 Plant = ss(Blackbox_model);
 [A,B,C,D] = ssdata(Plant);
 gainAngle = 0;
 gainPos   = 1;
-P = 1;
-I = 0;
-D = 0;
+% P = 1;
+% I = 0;
+% D = 0;
