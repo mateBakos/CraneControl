9013ec5f3b62f60fba9fda65fc4712f46c934158
Thomas Booij <thomasbooij95@gmail.com>
2018-06-06 15:46:47 +0200

    change filename

--- HelpFromWebsite/System identification/FilteringExample.m
+++ HelpFromWebsite/System identification/FilteringExample.m
@@ -1,5 +1,6 @@
  %close all 
  clear all
+ close all
  clc
 % Example 1:
     %   Zero-phase filter a noisy ECG waveform using an IIR filter.
@@ -16,12 +17,12 @@
     %clf(9)
     figure
     %load noisysignals x;                   % noisy waveform
-    [b,a] = butter(20,0.7,'low');           % IIR filter design
+    [b,a] = cheby1(1,10,0.6)%butter(20,0.1,'low');           % IIR filter design
     y = filtfilt(b,a,x);                    % zero-phase filtering
     y2 = filter(b,a,x);                     % conventional filtering
     
     axis([0 timeVector(end) -1 1])
-    figure
+    %figure
     plot(timeVector,[y ;y2],'LineWidth',2); grid on ; hold on;
     plot(timeVector,x,'k-');
     axis([0 0.2 -0.1 0.1]);
--- m-files/SimulinkPIDinit.m
+++ m-files/SimulinkPIDinit.m
@@ -6,3 +6,27 @@ gainPos   = 1;
 % P = 1;
 % I = 0;
 % D = 0;
+
+%% Observer-building
+
+rank(obsv(Plant))
+rank(ctrb(Plant))
+
+%% Pole placement
+t = 0:0.01:2;
+u = zeros(size(t));
+x0 = [0 1 0 0];
+
+p1 = -10 + 10i;
+p2 = -10 - 10i;
+p3 = -50;
+p4 = -0.01;
+K = place(A,B,[p1 p2 p3 p4]);
+sys_cl = ss(A-B*K,B,C,0);
+
+
+lsim(sys_cl,u,t,x0);
+
+xlabel('Time (sec)')
+ylabel('Ball Position (m)')
+
