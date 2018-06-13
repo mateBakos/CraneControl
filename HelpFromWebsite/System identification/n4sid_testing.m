1b5f34ba412b614fdbb10d4bb2cee2a75d383bb9
Thomas Booij <thomasbooij95@gmail.com>
2018-06-13 15:13:37 +0200

    remove helpfromwebsitefolder

--- HelpFromWebsite/System identification/n4sid_testing.m
+++
@@ -1,7 +0,0 @@
-clear all
-
-Bernoulli_1 = load(fullfile('..','Simulation_results','Bernoulli_1m_10.mat'),'Simulation_results')
-Bernoulli = Bernoulli_1.Simulation_results  
-
-
-plot_timeseries(Bernoulli);
